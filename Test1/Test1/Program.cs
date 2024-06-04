e9c25e7c6a0f112aff279d60e3af6b4ef175d5af
Raiyen Zayed Rakin <rakinaiub2023@gmail.com>
2024-06-04 08:40:50 +0600

    test

---
+++ Test1/Test1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = Console.ReadLine();
+            Console.WriteLine(name);
+        }
+    }
+}
