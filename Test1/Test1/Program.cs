cfa7abf20fd53ff731d309e3ce5fb25d4fd0ab58
Raiyen Zayed Rakin <rakinaiub2023@gmail.com>
2024-06-11 09:10:07 +0600

    donno

--- Test1/Test1/Program.cs
+++ Test1/Test1/Program.cs
@@ -1,29 +1,34 @@
 ﻿using System;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 
 namespace Test1
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-        //    System.Console.WriteLine("Hello world");
+            //System.Console.WriteLine("Hello world");
 
-        //    string name = "rakin";
+            //    string name = "rakin";
 
-        //    Console.WriteLine($"my name is {name}");
+            //    Console.WriteLine($"my name is {name}");
 
 
-            int[] source = { 1, 2, 3 };
-            
-            var query = from item in source where item <= 2 select item;
+            //int[] source = { 1, 2, 3 };
 
-            foreach( var item in query )
-            {
-                Console.WriteLine(item);
-            }
-            Console.ReadLine();
+            //var query = from item in source where item <= 2 select item;
+
+            //foreach( var item in query )
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+
+
+
         }
     }
 }
