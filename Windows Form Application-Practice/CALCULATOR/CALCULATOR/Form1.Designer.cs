b0f0f26f2bff9e7e27cef9a9c14a5d6129317836
Raiyen Zayed Rakin <rakinaiub2023@gmail.com>
2024-06-19 15:39:22 +0600

    WinForms

---
+++ Windows Form Application-Practice/CALCULATOR/CALCULATOR/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace CALCULATOR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.add = new System.Windows.Forms.Button();
+            this.div = new System.Windows.Forms.Button();
+            this.multi = new System.Windows.Forms.Button();
+            this.subs = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(276, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(419, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BASIC CALCULATOR";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(117, 263);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(346, 34);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter Second Number :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Modern No. 20", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(117, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(340, 34);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Enter First Number   :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(497, 161);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(332, 34);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(497, 263);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(332, 34);
+            this.textBox2.TabIndex = 4;
+            // 
+            // add
+            // 
+            this.add.Font = new System.Drawing.Font("Modern No. 20", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.add.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(191)))), ((int)(((byte)(155)))));
+            this.add.Location = new System.Drawing.Point(123, 357);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(138, 35);
+            this.add.TabIndex = 5;
+            this.add.Text = "ADDISION";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // div
+            // 
+            this.div.Font = new System.Drawing.Font("Modern No. 20", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.div.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(191)))), ((int)(((byte)(155)))));
+            this.div.Location = new System.Drawing.Point(703, 357);
+            this.div.Name = "div";
+            this.div.Size = new System.Drawing.Size(126, 35);
+            this.div.TabIndex = 6;
+            this.div.Text = "DIVISION";
+            this.div.UseVisualStyleBackColor = true;
+            this.div.Click += new System.EventHandler(this.div_Click);
+            // 
+            // multi
+            // 
+            this.multi.Font = new System.Drawing.Font("Modern No. 20", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.multi.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(191)))), ((int)(((byte)(155)))));
+            this.multi.Location = new System.Drawing.Point(463, 357);
+            this.multi.Name = "multi";
+            this.multi.Size = new System.Drawing.Size(232, 35);
+            this.multi.TabIndex = 7;
+            this.multi.Text = "MULTIPLICATION";
+            this.multi.UseVisualStyleBackColor = true;
+            this.multi.Click += new System.EventHandler(this.multi_Click);
+            // 
+            // subs
+            // 
+            this.subs.Font = new System.Drawing.Font("Modern No. 20", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subs.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(191)))), ((int)(((byte)(155)))));
+            this.subs.Location = new System.Drawing.Point(267, 357);
+            this.subs.Name = "subs";
+            this.subs.Size = new System.Drawing.Size(190, 35);
+            this.subs.TabIndex = 8;
+            this.subs.Text = "SUBSTRACTION";
+            this.subs.UseVisualStyleBackColor = true;
+            this.subs.Click += new System.EventHandler(this.subs_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Modern No. 20", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(318, 442);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Visible = false;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(191)))), ((int)(((byte)(155)))));
+            this.ClientSize = new System.Drawing.Size(962, 544);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.subs);
+            this.Controls.Add(this.multi);
+            this.Controls.Add(this.div);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CALCULATOR_DEMO";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button div;
+        private System.Windows.Forms.Button multi;
+        private System.Windows.Forms.Button subs;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Windows Form Application-Practice/CALCULATOR/CALCULATOR/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CALCULATOR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //public int get_nums()
+        //{
+        //    int num1 = Convert.ToInt32(textBox1.Text);
+        //    int num2 = Convert.ToInt32(textBox2.Text);
+
+        //    return { num1, num2};
+        //}
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(textBox1.Text);
+            int num2 = Convert.ToInt32(textBox2.Text);
+
+            int result = num1 + num2;
+
+            //MessageBox.Show($"Your result is : {result.ToString()}");
+
+            label4.Text = $"Your Addition Result is : {result.ToString()}";
+            label4.Visible = true;
+
+        }
+
+       
+
+        private void subs_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(textBox1.Text);
+            int num2 = Convert.ToInt32(textBox2.Text);
+
+            int result = num1 - num2;
+            label4.Text = $"Your Substraction Result is : {result.ToString()}";
+            label4.Visible = true;
+            //MessageBox.Show($"Your result is : {result.ToString()}");
+        }
+       
+        private void multi_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(textBox1.Text);
+            int num2 = Convert.ToInt32(textBox2.Text);
+
+            int result = num1 * num2;
+            label4.Text = $"Your Multiplication Result is : {result.ToString()}";
+            label4.Visible = true;
+            //MessageBox.Show($"Your result is : {result.ToString()}");
+        }
+        private void div_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(textBox1.Text);
+            int num2 = Convert.ToInt32(textBox2.Text);
+
+            int result = num1 / num2;
+
+            label4.Text = $"Your Division Result is : {result.ToString()}";
+            label4.Visible = true;
+            //MessageBox.Show($"Your result is : {result.ToString()}");
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
