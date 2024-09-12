f685c34da302d929d640146731a9e3c540f2253f
Raiyen Zayed Rakin <rakinaiub2023@gmail.com>
2024-09-12 15:31:45 +0600

    new WinForms

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text += button1.Text;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox2.Text += button3.Text;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox2.Text += button6.Text;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text += button2.Text;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox2.Text += button4.Text;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+            textBox2.Text += button7.Text;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox2.Text += button8.Text;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox2.Text += button5.Text;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox2.Text += button9.Text;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox2.Text += button10.Text;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            textBox2.Text += button12.Text;
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            textBox2.Text += button15.Text;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox2.Text += button11.Text;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            textBox2.Text += button13.Text;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            textBox2.Text += button14.Text;
+        }
+    }
+ }
